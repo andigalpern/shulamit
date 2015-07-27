2505aa28b0611bcbd680476c58514f054446d8a4
Ben <benjaminshrimpton@yahoo.co.uk>
2015-07-27 09:33:04 -0400

    Layout adjustment on PAST EXPO page

--- wp-content/themes/shulamit/page-past-exhibitions.php
+++ wp-content/themes/shulamit/page-past-exhibitions.php
@@ -8,38 +8,34 @@
     </div>
   </header>
   
-  
 
 <?php $loop = new WP_Query( array( 'post_type' => 'exhibition' ,  'posts_per_page' => 16 , 'cat'=>'8'  ) ); ?>
 <?php while ( $loop->have_posts() ) : $loop->the_post() ; ?>  
 
-
-      
-  <section class="row post"> 
+  <article class="row post"> 
     <figure class="col-sm-6">
-        <a href="<? the_permalink(); ?>">
-          <?php
+      <a href="<? the_permalink(); ?>">
+        <?php
             if ( has_post_thumbnail() ){
               the_post_thumbnail('medium_cropped', array('class' => 'img-responsive')); 
-             }
-             else {
-               echo '<img src="http://placehold.it/600x400" class="img-responsive"/>';
-             }
-          ?>
-        </a> 
+            }
+            else {
+              echo '<img src="http://placehold.it/600x400?text=Unavailable" class="img-responsive"/>';
+            }
+        ?>
+      </a> 
     </figure>
-      <section class="col-sm-6">
-        <header class="post-header no-date">
-          <span class="bold slug">Past Exhibition</span>
-         <h2 class="heavy"><? the_title();?></h2>
-         <h3 class="heavy"><? the_field('sub_title'); ?></h3>
-        </header>
-        <section class="content">
-        <? the_excerpt();?>
-        </section>
-    </secti
-    
-    
+    <section class="col-sm-6">
+      <header class="post-header no-date">
+        <span class="bold slug">Past Exhibition</span>
+       <h2 class="heavy"><? the_title();?></h2>
+       <h3 class="heavy"><? the_field('sub_title'); ?></h3>
+      </header>
+      <section class="content">
+      <? the_excerpt();?>
+      </section>
+    </section>
+  </article>
       
 <?php endwhile;?> 
 <?php wp_reset_query();?> 
