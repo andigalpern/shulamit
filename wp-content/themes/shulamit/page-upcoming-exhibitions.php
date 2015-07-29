be5932125242af82e74cdc2027069e5d941cd298
Ben <benjaminshrimpton@yahoo.co.uk>
2015-07-29 11:32:45 -0400

    no link if no images

--- wp-content/themes/shulamit/page-upcoming-exhibitions.php
+++ wp-content/themes/shulamit/page-upcoming-exhibitions.php
@@ -11,7 +11,7 @@
     </div>
   </header>
   
-<?php $loop = new WP_Query( array( 'post_type' => 'exhibition' ,  'posts_per_page' => '9' , 'cat'=>'9' ) ); ?>
+<?php $loop = new WP_Query( array( 'post_type' => 'exhibition' ,  'posts_per_page' => '30' , 'cat'=>'9' ) ); ?>
 <?php while ( $loop->have_posts() ) : $loop->the_post() ; ?>
 
  <section class="row">
@@ -27,8 +27,21 @@
       <section class="col-sm-6">
         <header>
           <span class="bold slug">Upcoming</span>
-          <h2 class="heavy"><a href="<? the_permalink(); ?>"><? the_title(); ?>:</a></h2>
-          <h3 class="heavy"><a href="<? the_permalink(); ?>"><? the_field('sub_title'); ?></a></h3>
+          <h2 class="heavy">
+              <? if(has_post_thumbnail()):?>
+              <a href="<? the_permalink(); ?>"><? the_title(); ?>:</a>
+              <? else: ?> 
+               <? the_title(); ?>: 
+              <? endif; ?>
+             </h2>
+          <h3 class="heavy">
+            <? if(has_post_thumbnail()):?>
+            <a href="<? the_permalink(); ?>"><? the_field('sub_title'); ?></a>
+            <? else: ?> 
+            <? the_field('sub_title'); ?>  
+            <? endif; ?>  
+            </a>
+          </h3>
           <time><?php the_field('dates'); ?></time>
         </header>
         
@@ -44,35 +57,7 @@
 <?php endwhile; ?>
 <?php wp_reset_query();?> 
   
- <section class="row">
-    <?php $loop = new WP_Query( array( 'post_type' => 'exhibition' ,  'posts_per_page' => 16,   'offset' => '9', 'cat'=>'9'  ) ); ?>
-    <?php while ( $loop->have_posts() ) : $loop->the_post() ; ?>  
-      <article class="col-sm-4 post">
-        <figure class="">
-          <a href="<? the_permalink(); ?>">
-            <?php the_post_thumbnail('medium_cropped', array('class' => 'img-responsive')); ?>
-          </a>
-        </figure>
-        
-        <section class="">
-          <header>
-          <span class="bold slug">Upcoming</span>
-          <h2 class="heavy"><? the_title(); ?>:</h2>
-          <h3 class="heavy"><? the_field('sub_title'); ?></h3>
-          <time><?php the_field('dates'); ?></time>
-          </header>
-          
-          <section class="content">
-            <?php the_content();?>
-          </section>
-        </section>
-      </article>
-    <?php endwhile;?> 
-    <?php wp_reset_query();?> 
-    
-    
- </section><!-- end row -->
- 
+
  
 </section><!-- end container -->
 
